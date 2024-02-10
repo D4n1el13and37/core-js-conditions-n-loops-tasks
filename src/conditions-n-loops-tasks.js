37370f3b05a7c89aea06b3cda91240ca4f0fdf13
D4n1el13and37 <daniel837@mail.ru>
2024-02-10 13:35:55 +0300

    feat: solve shuffleChar without best time

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -431,8 +431,24 @@ function sortByAsc(arr) {
  *  '012345', 3 => '024135' => '043215' => '031425'
  *  'qwerty', 3 => 'qetwry' => 'qtrewy' => 'qrwtey'
  */
-function shuffleChar(/* str, iterations */) {
-  throw new Error('Not implemented');
+function shuffleChar(str, iterations) {
+  let answer = str;
+
+  for (let i = 0; i < iterations; i += 1) {
+    let head = '';
+    let tail = '';
+
+    for (let j = 0; j < answer.length; j += 1) {
+      if (j % 2 === 0) {
+        head += answer[j];
+      } else {
+        tail += answer[j];
+      }
+    }
+    answer = `${head}${tail}`;
+  }
+
+  return answer;
 }
 
 /**
